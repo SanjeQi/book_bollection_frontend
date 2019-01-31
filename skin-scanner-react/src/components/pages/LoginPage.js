28200f32add1b15d1603403cd132ff3c560013e5
github username <github email address>
2019-01-31 14:50:32 +0000

    add comp plus axios

--- skin-scanner-react/src/components/pages/LoginPage.js
+++ skin-scanner-react/src/components/pages/LoginPage.js
@@ -1,9 +1,14 @@
 import React from "react";
 import LoginForm from "../forms/LoginForm";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
   submit = data => {
-    console.log(data);
+    //history is provided by router
+    //login will be available after I connect to redux
+    this.props.login(data).then(() => this.props.history.push("/"));
   };
 
   render() {
@@ -16,4 +21,14 @@ class LoginPage extends React.Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
